ca988d195b7d330227b3cc589028165691f3eb60
Vladyslav <vladsw764@icloud.com>
2023-05-14 18:55:59 +0300

    added Student model

---
+++ src/main/java/com/example/mongo_db_course/model/Student.java
@@ -0,0 +1,28 @@
+package com.example.mongo_db_course.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Document
+@Builder
+public class Student {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    @Indexed(unique = true)
+    private String email;
+    private Gender gender;
+    private Address address;
+    private List<String> favouriteSubjects;
+    private BigDecimal totalSpentInBooks;
+    private LocalDateTime created;
+}
