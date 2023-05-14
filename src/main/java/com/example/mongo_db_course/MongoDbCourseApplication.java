97c599cfd3f3525ad9633c4f067c54fe0fae6862
Vladyslav <vladsw764@icloud.com>
2023-05-14 11:59:41 +0300

    Init commit

---
+++ src/main/java/com/example/mongo_db_course/MongoDbCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongo_db_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongo_db_course/MongoDbCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongo_db_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
