16b0d5e9b80f5165e711b647330c90f5d5eff04a
Vladyslav <vladsw764@icloud.com>
2023-05-14 18:55:08 +0300

    added Gender model

---
+++ src/main/java/com/example/mongo_db_course/model/Address.java
@@ -0,0 +1,12 @@
+package com.example.mongo_db_course.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Address {
+    private String country;
+    private String postCode;
+    private String city;
+}
