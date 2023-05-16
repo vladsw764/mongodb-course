2b534115b36be7af40a108191d76b4169213c89a
Vladyslav <vladsw764@icloud.com>
2023-05-16 13:11:46 +0300

    added StudentRepository

--- src/main/java/com/example/mongo_db_course/repository/StudentRepository.java
+++ src/main/java/com/example/mongo_db_course/repository/StudentRepository.java
@@ -3,6 +3,9 @@ package com.example.mongo_db_course.repository;
 import com.example.mongo_db_course.model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends MongoRepository<Student, String> {
+    Optional<Student> findStudentByEmail(String email);
 
 }
